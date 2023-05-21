7d52b2ac55095c69fd152f83bf347f5e1e9c6e4c
kareemvga <karemm@kiebot.com>
2023-05-21 20:36:56 +0530

    removed config file

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,28 +1,15 @@
 import React, { useState } from "react";
 import "./Register.css";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { initializeApp } from "firebase/app";
 
 function Register({ setRegister }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBsuyALkiFj2sf48hvBa0T2pABRGYw9QwA",
-    authDomain: "porto-bc83c.firebaseapp.com",
-    projectId: "porto-bc83c",
-    storageBucket: "porto-bc83c.appspot.com",
-    messagingSenderId: "840938555551",
-    appId: "1:840938555551:web:6492d6500cdc4d93affaaa",
-    measurementId: "G-HZC66R9KMZ",
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-
   const handleRegister = async (event) => {
     event.preventDefault();
     try {
+      const auth = getAuth();
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
